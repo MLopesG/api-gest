2b7378bcbcf3fd650bc082bed5d93df20c99563e
ODERCO\marcos.goncalves <marcos.goncalves@oderco.com.br>
2021-12-03 10:59:22 -0300

    Novas consultas a SQLS.

--- consultas.sql
+++ consultas.sql
@@ -146,6 +146,96 @@ inner join manutencao_tipo mt on mt.id = m.manutencao_tipo_id
 left join veiculo vs on vs.id = m.veiculo_id_temporario
 order by m.id desc;
 ----------------------------------------------------
+-- Consultas previas detalhadas
+select 
+	mp.id,
+	mp.km_previsao,
+	mp.data_previsao,
+	mt.descricao as categoria_manutencao,
+	(
+		case
+			when mp.is_aprovado then 'Aprovado.'
+			else 'Cancelado.'
+		end
+	) as status_previsao,
+	m.km_atual,
+	m.descricao, 
+	m.valor_pago,
+	mt.descricao as categoria_manutencao,
+	(
+		case
+			when m.is_finalizado then 'Concluido.'
+			when m.is_andamento then 'Em Andamento.'
+			when m.is_cancelado then 'Cancelado.'
+			else 'Não identificado.'
+		end
+	) as status_manutencao,
+	v.id as id_veiculo_manutencao,
+	v.placa as veiculo_manutencao,
+	(
+		case
+			when vs.descricao is null then 'Veiculo não substituido.'
+			else vs.placa
+		end 
+	) as veiculo_substituto,
+	vs.id as id_veiculo_substituto,
+	u.id as usuario_id,
+	u.nome as usuario,
+	m.created_at as manutencao_realizado,
+	mp.created_at as previsao_aberta_em,
+	mp.updated_at as ultima_atualizacao_registro
+FROM manutencao_previsao mp
+inner join manutencao m on m.id = mp.ultima_manutencao_id 
+inner join veiculo v on v.id = m.veiculo_id 
+left  join usuario u on u.id = m.usuario_id 
+inner join manutencao_tipo mt on mt.id = m.manutencao_tipo_id 
+left join veiculo vs on vs.id = m.veiculo_id_temporario
+where mp.ultima_manutencao_id = 1;
+----------------------------------------------------
+-- Listar todas previsões do dia
+select 
+	mp.id,
+	mp.km_previsao,
+	mp.data_previsao,
+	mt.descricao as categoria_manutencao,
+	(
+		case
+			when mp.is_aprovado then 'Aprovado.'
+			else 'Cancelado.'
+		end
+	) as status_previsao,
+	m.descricao, 
+	v.placa as veiculo_manutencao,
+	mp.created_at as previsao_aberta_em,
+	mp.updated_at as ultima_atualizacao_registro
+FROM manutencao_previsao mp
+inner join manutencao m on m.id = mp.ultima_manutencao_id 
+inner join veiculo v on v.id = m.veiculo_id 
+inner join manutencao_tipo mt on mt.id = m.manutencao_tipo_id 
+where date(mp.created_at ) = current_date
+order by mp.created_at desc;
+-- Listar todas previsões
+select 
+	mp.id,
+	mp.km_previsao,
+	mp.data_previsao,
+	mt.descricao as categoria_manutencao,
+	(
+		case
+			when mp.is_aprovado then 'Aprovado.'
+			else 'Cancelado.'
+		end
+	) as status_previsao,
+	m.descricao, 
+	v.placa as veiculo_manutencao,
+	mp.created_at as previsao_aberta_em,
+	mp.updated_at as ultima_atualizacao_registro
+FROM manutencao_previsao mp
+inner join manutencao m on m.id = mp.ultima_manutencao_id 
+inner join veiculo v on v.id = m.veiculo_id 
+inner join manutencao_tipo mt on mt.id = m.manutencao_tipo_id 
+order by mp.created_at desc;
+----------------------------------------------------
 -- Consultas básicas
 select * from usuario;
 select * from veiculo;
@@ -154,4 +244,5 @@ select * from produto;
 select * from movimento_produto;
 select * from manutencao;
 select * from manutencao_tipo;
+select * from manutencao_previsao;
 ----------------------------------------------------
